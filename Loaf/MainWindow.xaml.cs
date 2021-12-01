5919ceb2709a15dd8a05355b36c32986f69f0fd9
DinoChan <dino.chan@live.com>
2021-12-01 16:21:48 +0800

    update some code

--- Loaf/MainWindow.xaml.cs
+++ Loaf/MainWindow.xaml.cs
@@ -1,25 +1,11 @@
-﻿using CommunityToolkit.WinUI.UI;
-using Microsoft.UI;
+﻿using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.ViewManagement;
-using System.Windows.Input;
 using System.Runtime.InteropServices;
-using System.Diagnostics;
-using System.Threading;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
